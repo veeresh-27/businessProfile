3ed2c2ee25afb7f99e6615f6704dde15f5dbd155
veeresh-27 <veereshrm789@gmail.com>
2022-11-02 20:19:30 +0530

    Header creation initiated

---
+++ components/header/index.js
@@ -0,0 +1,13 @@
+export function Header(){
+    return(
+<div className="sticky top-0 z-50 flex items-center justify-between w-full bg-background md:px-5 lg:px-14 xl:px-20 h-14 ">
+    <div
+        className="p-3 text-3xl font-bold cursor-pointer text-primary sm:text-2xl md:text-3xl lg:text-4xl">
+        <h1>My&nbsp;Diary</h1>
+        <div>
+
+        </div>
+    </div>
+</div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {Header} from "../components/header";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
+        <Header/>
       <Head>
         <title>Financial Advisor</title>
         <meta name="description" content="Finance Management | House-Insurance Health-Insurance | Loans" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
             <div className='animate-pulse '>Work in Progress...</div>
