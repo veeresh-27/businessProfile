c53ebabb5f2088260cc59efc06dd616a21ba2b75
Veeresh R M <veereshrm789@gmail.com>
2022-11-02 16:06:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Work in Progress
         </h1>
 
 
