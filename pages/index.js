daed877f09645ee9449d4ae9b17ca9d30404a929
veeresh-27 <veereshrm789@gmail.com>
2022-11-02 15:10:40 +0530

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
