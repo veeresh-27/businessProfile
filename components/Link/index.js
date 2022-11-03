210c01fcb2652ee0b73592f541ee18cafc6e774d
veeresh-27 <veereshrm789@gmail.com>
2022-11-03 17:08:19 +0530

    Responsive nav added

--- components/Link/index.js
+++ components/Link/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { NavLink } from "react-router-dom";
 
-export const Link = () => {
+export const Link = ({isOnMobile=false,setMobNav=null}) => {
 
     const navLinkTitle = [
         {
@@ -18,17 +18,17 @@ export const Link = () => {
         },
         {
             name: "Contact-Us",
-            style: 'border-2 border-green-600 cursor-pointer p-1 hover:bg-green-600 hover:text-white transition-all ease-in-out duration-500',
+            style: 'border-2 border-green-600 cursor-pointer p-0.5 hover:bg-green-600 hover:text-white transition-all ease-in-out duration-500 rounded',
         },
 
     ];
 
     return (
 
-        <div className="relative flex items-center justify-between gap-2 text-l text-gray-700 font-medium">
+        <div className={`relative flex items-center justify-between gap-2 text-l text-gray-700 font-medium p-3 ${isOnMobile? 'flex-col':null}`}>
 
             {navLinkTitle.map(({ name,title,style }, index) => (
-                <div title={title?`${title}`:null} key={index}  className={style?`${style}`:`cursor-pointer`}>
+                <div title={title?`${title}`:null} key={index} onClick={setMobNav? ()=> setMobNav(false):null}  className={style?`${style}`:`cursor-pointer`}>
                     {name}
                 </div>
             ))}
--- components/header/index.js
+++ components/header/index.js
@@ -1,19 +1,20 @@
 // import { useNavigate } from "react-router-dom";
 import { Link } from  "../Link"
 import {IconBurger} from "../../public/icons";
+import {useState} from "react";
 
 export function Header(){
+    const [mobNav,setMobNav] = useState(false);
     return(
-<div className="sticky top-0 z-50 flex items-center justify-between w-full bg-background md:px-5 lg:px-14 xl:px-20 h-14 ">
+<div className="sticky top-0 z-50 flex items-center justify-between w-full bg-background md:px-5 lg:px-14 xl:px-20 h-14 relative">
 <div
     className="p-3 text-xl font-bold cursor-pointer text-primary sm:text-2xl md:text-2xl lg:text-3xl">
     <h1>My&nbsp;Financer</h1>
-
-
 </div>
     <div className="hidden md:flex md:items-center transition-all ease-in duration-500  gap-2 "><Link/></div>
-    <div className='md:hidden p-3 cursor-pointer'><IconBurger/></div>
-</div>
+        <div className='md:hidden p-3 cursor-pointer' onClick={()=>setMobNav(!mobNav)}><IconBurger/></div>
+        <div className={`flex flex-col absolute w-1/3 bg-gray-900 top-12 right-0 p-3  ${mobNav ? 'translate-x-0': 'translate-x-full'} translate-all ease-in-out duration-200`}><Link isOnMobile setMobNav/></div>
 
+</div>
     );
 }
